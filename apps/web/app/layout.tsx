d8d13f448b2446e6c010f85c905b90b2a1612288
DanPace725 <118482982+DanPace725@users.noreply.github.com>
2024-03-21 22:26:12 -0700

    Updates

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -10,21 +10,6 @@ const title =
 const description =
   "Meet Mindi, the fusion of conversational AI and advanced note-taking, designed to remember so you don't have to. Effortlessly toggle between engaging chats and organized note-taking. With Mindi, every conversation enriches your notes, creating a dynamic, searchable memory bank. Mindi doesn't just respond; it recalls, stores, and organizes your thoughts and interactions seamlessly";
 
-export const metadata: Metadata = {
-  title,
-  description,
-  openGraph: {
-    title,
-    description,
-  },
-  twitter: {
-    title,
-    description,
-    card: "",
-    creator: "",
-  },
-  metadataBase: new URL(""),
-};
 
 export const viewport: Viewport = {
   themeColor: "#ffffff",
